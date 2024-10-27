f83aea3df97732e3f91cfaa7db09c224a7ea5c1b
devSharma14 <devs140124@gmail.com>
2024-10-27 22:54:21 +0530

    middleware added

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { signup, login } from "../Controllers/AuthController.js";
+import userVerification from "../middleware/AuthMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", login);
+router.post("/", userVerification);
 router.get("/test", (req, res) => {
     return res.json({ message: "Signup route is working!" });
 });
